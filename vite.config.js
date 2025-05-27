000753a1f2d486cbc90a619d2144c547c1293544
rtinn <135967326+rtinn@users.noreply.github.com>
2025-05-27 14:36:19 +0300

    conf
    conf

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // ← ici on change
+  base:'/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
